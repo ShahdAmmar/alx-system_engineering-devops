40d89a56e7abf5994d8d4fc62c3bbdc0cab76867
root <shahdammar413@gamil.com>
2024-08-16 12:12:42 -0700

    0-gather_data_from_an_API.py script

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/user/bin/python3
+import requests
+import sys
+
+
+def get_employee_todo_list_progress(emp_id):
+    """ This function gets information about the employee todo list progress
+    and prints the completed tasks
+    Args:
+        emp_id: the employee ID
+    """
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user = requests.get(url + f'users/{emp_id}')
+    user_json = user.json()
+    emp_name = user_json.get('name')
+    if emp_name is None:
+        print('This employee doesn\'t exist. You entered invalid ID.')
+        sys.exit()
+
+    parameters = {'userId': emp_id}
+    todos = requests.get(url + f'todos', params=parameters)
+    todos_json = todos.json()
+
+    completed_tasks = [task for task in todos_json if task.get('completed')]
+    no_comp_tasks = len(completed_tasks)
+    no_totl_tasks = len(todos_json)
+    print(f'Employee {emp_name} is done with',
+          f'tasks ({no_comp_tasks}/{no_totl_tasks}):')
+    for task in completed_tasks:
+        print('\t', task.get('title'))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Usage: script_name <employee_id>')
+    else:
+        get_employee_todo_list_progress(sys.argv[1])
