3f1a7cf9bf7a4fa1ea5623920bac6d3a01769d5c
root <shahdammar413@gamil.com>
2024-08-16 14:00:00 -0700

    editting 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,28 +11,26 @@ def get_employee_todo_list_progress(emp_id):
     Args:
         emp_id: the employee ID
     """
-    if __name__ == "__main__":
-        url = "https://jsonplaceholder.typicode.com/"
-        emp_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
 
-        user = requests.get(url + f'users/{emp_id}')
-        user_json = user.json()
-        emp_name = user_json.get('name')
-        if emp_name is None:
-            print('This employee doesn\'t exist. You entered invalid ID.')
-            sys.exit()
+    user = requests.get(url + f'users/{emp_id}')
+    user_json = user.json()
+    if user_json == {}:
+        print('This employee doesn\'t exist. You entered invalid ID.')
+        sys.exit()
+    emp_name = user_json.get('name')
 
-        parameters = {'userId': emp_id}
-        todos = requests.get(url + f'users/{emp_id}/todos')
-        todos_json = todos.json()
+    parameters = {'userId': emp_id}
+    todos = requests.get(url + f'users/{emp_id}/todos')
+    todos_json = todos.json()
 
-        completed_tasks = [tsk for tsk in todos_json if tsk.get('completed')]
-        comp_tasks = len(completed_tasks)
-        totl_tasks = len(todos_json)
-        print('Employee {} is done with tasks({}/{}):'.
-              format(emp_name, comp_tasks, totl_tasks))
-        for task in completed_tasks:
-            print(f"\t {task.get('title')}")
+    completed_tasks = [tsk for tsk in todos_json if tsk.get('completed')]
+    comp_tasks = len(completed_tasks)
+    totl_tasks = len(todos_json)
+    print('Employee {} is done with tasks({}/{}):'.
+          format(emp_name, comp_tasks, totl_tasks))
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
 
 
 if __name__ == "__main__":
