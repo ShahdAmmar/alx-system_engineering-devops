d3548e42a21490e9ea8189aa24e8bb10a7926119
root <shahdammar413@gamil.com>
2024-08-23 03:50:14 -0700

    0-subs.py commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import requests
+""" Module that returns the number of subscribers for a given subreddit """
+
+
+def number_of_subscribers(subreddit):
+    """ This functions returns the total number
+            of subscribers for a given subreddit.
+    Args:
+        subreddit: name of the subreddit
+    Return:
+        No. subscribers for a given subreddit or 0 if invalid subreddit
+    """
+    headers = {'User-Agent': 'My-User-Agent'}
+    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                            headers=headers,
+                            allow_redirects=False)
+    if response.status_code > 299:
+        return 0
+
+    subreddit = response.json()
+    no_subscribers = subreddit.get('data').get('subscribers')
+    return no_subscribers
